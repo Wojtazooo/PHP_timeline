821dcf09c9322b783625df3a5ac72ba36cf7815e
WOJTEK-STACJO\wojte <wojtekulaski@gmail.com>
2024-11-10 10:43:04 +0100

    environmental variables to allow configure database connection

--- src/api/database/DatabaseConnection.php
+++ src/api/database/DatabaseConnection.php
@@ -4,10 +4,10 @@ mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 
 function connectToDatabase(): mysqli
 {
-    $host = 'localhost';
-    $user = 'root';
-    $password = 'admin';
-    $dbname = 'eventsdb';
+    $host = getenv('DB_HOST') ?: 'localhost';
+    $user = getenv('DB_USERNAME') ?: 'root';
+    $password = getenv('DB_PASSWORD') ?: 'admin';
+    $dbname = getenv('DB_DATABASE') ?: 'eventsdb';
 
     $mysqli = new mysqli($host, $user, $password, $dbname);
 
